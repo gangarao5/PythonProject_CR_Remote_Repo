83255690bbd026f4f7e8d93b9dd6166829173db0
Veera Gangarao <veera_gangarao@epam.com>
2026-01-09 20:06:12 +0530

    pipeline committed

--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_get_users_api():
-    response = requests.get("https://www.flipkart.com/")
+    response = requests.get("https://www.flipkart.om/")
     # response = requests.get("https://reqres.in/api/users?page=2")
 
     assert response.status_code == 529
