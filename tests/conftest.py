bad42422d0dce72b5ac850259597d97d7bacac74
Veera Gangarao <veera_gangarao@epam.com>
2026-01-09 19:32:44 +0530

    pipeline committed

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")  # Jenkins safe
+    options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    yield driver
+
+    driver.quit()
--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -1,20 +1,9 @@
-# import pytest
-import time
-
-from selenium import webdriver
-from pages.login_page import LoginPage
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-def test_ui_login_page():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    page = LoginPage(driver)
-    page.open_url("https://www.flipkart.com/")
-    time.sleep(5)
-    title = page.get_title()
-    print(title)
-    assert "Online Shopping" in page.get_title()
-
-    driver.quit()
+def test_flipkart_login_page_title(driver):
+    try:
+        driver.get("https://www.flipkart.com/")
+        title = driver.title
+        print(title)
+        assert "Online Shopping" in title
+    except Exception:
+        driver.save_screenshot("reports/flipkart_login_failure.png")
+        raise
