318c3003a232f54d211ff5b577155c2ec51938e1
Veera Gangarao <veera_gangarao@epam.com>
2026-01-10 23:50:38 +0530

    pipeline committed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -16,3 +18,24 @@ def driver():
     yield driver
 
     driver.quit()
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+
+        if driver:
+            screenshots_dir = "screenshots"
+            os.makedirs(screenshots_dir, exist_ok=True)
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            screenshot_name = f"{item.name}_{timestamp}.png"
+            screenshot_path = os.path.join(screenshots_dir, screenshot_name)
+
+            driver.save_screenshot(screenshot_path)
+            print(f"\n Screenshot saved: {screenshot_path}")
--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -1,9 +1,5 @@
 def test_flipkart_login_page_title(driver):
-    try:
-        driver.get("https://www.flipkart.com/")
-        title = driver.title
-        print(title)
-        assert "Online Shopping" in title
-    except Exception:
-        driver.save_screenshot("reports/flipkart_login_failure.png")
-        raise
+    driver.get("https://www.flipkart.com/")
+    title = driver.title
+    print(title)
+    assert "Online Shopping" in title
